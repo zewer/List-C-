d080f7bfadad5515f2943e5750424bb49d9a3197
Kutaiev Serhii <zewer1993@gmail.com>
2017-10-15 01:48:57 +0300

    Added second Hello World

--- test/Program.cs
+++ test/Program.cs
@@ -11,6 +11,7 @@ namespace test
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
