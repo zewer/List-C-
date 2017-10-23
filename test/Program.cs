de8ea9e79fb8e7af3656fda4c6bfe9d196c53eeb
Kutaiev Serhii <zewer1993@gmail.com>
2017-10-23 21:20:17 +0300

    Pet_Shop

--- test/Program.cs
+++ test/Program.cs
@@ -4,14 +4,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test
+namespace Shop
 {
+    abstract class Pet
+    {
+        public string Name;
+        public string Breed;
+
+        public void Introduce()
+        {
+            Console.WriteLine("I'm {0} of {1}.I'm a {2}", Name, Breed, GetType().Name);
+        }
+    }
+    class PetShop
+    {
+        List<Pet> pets = new List<Pet>();
+
+        public void AddPet(Pet animal)
+        {
+            pets.Add(animal);
+        }
+
+        public void IntroduceAll()
+        {
+            try
+            {
+                foreach (var p in pets)
+                    p.Introduce();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong!", e);
+            }
+        }
+    }
+    class Dog : Pet
+    {
+        public Dog(string n, string b) : base()
+        {
+            Name = n;
+            Breed = b;
+        }
+
+    }
+    class Cat : Pet
+    {
+        public Cat(string n, string b) : base()
+        {
+            Name = n;
+            Breed = b;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hello, World!");
+
+            PetShop petshop = new PetShop();
+            petshop.AddPet(new Dog("Rex", "Fex"));
+            petshop.AddPet(new Cat("Mura", "Home"));
+            petshop.AddPet(new Dog("Bim", "Jef"));
+            petshop.IntroduceAll();
+            Console.ReadLine();
         }
     }
 }
